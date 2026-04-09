39d14b219b0fd67659a652de9510255baa46ccb0
katebksm-jpg <katebksm@gmail.com>
2026-04-09 15:13:06 +0000

    initial commit

---
+++ HW7.R
@@ -0,0 +1,7 @@
+ghg <- read.csv("/cloud/project/activity07/Deemer_GHG_Data.csv")
+ETdata <- read.csv("/cloud/project/activity07/ETdata.csv")
+
+library(dplyr)
+library(ggplot2)
+library(olsrr)
+library(PerformanceAnalytics)
